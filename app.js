dcf04facaecb4a852ac0ffe56630381004b471c6
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-05-29 14:18:03 +0200

    iterations 1 to bonus it 5

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const { engine } = require('express/lib/application');
 
 const hbs = require('hbs');
+const async = require('hbs/lib/async');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// hbs
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+//app.set("view engine", 'hbs')
 
 // Add the route handlers here:
 
@@ -22,12 +28,26 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res) => {
-  res.render('beers')
-})
+app.get('/beers', async (req, res) => {
+  try {
+    const apiBeers = await punkAPI.getBeers();
+    const firstTwentyFive = apiBeers.slice(0, 25)
+    res.render('beers', { firstTwentyFive });
+    //console.log(firstTwentyFive)
+  } catch (error) {
+    //console.log(error)
+  }
+});
 
-app.get('/random-beer', (req, res) => {
-  res.render('randomBeer')
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeer = await punkAPI.getRandom()
+    res.render('randomBeer', { randomBeer });
+    console.log(randomBeer)
+  } catch (error) {
+
+  }
+
 })
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
